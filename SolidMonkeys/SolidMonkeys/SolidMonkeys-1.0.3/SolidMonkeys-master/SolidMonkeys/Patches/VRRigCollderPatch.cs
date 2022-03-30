ea4217bf3398c46f1f751fc7590953ec50f3479e
YesMonke <96830968+YesMonke@users.noreply.github.com>
2022-03-30 19:12:00 -0400

    Add files via upload

---
+++ SolidMonkeys/SolidMonkeys/SolidMonkeys-1.0.3/SolidMonkeys-master/SolidMonkeys/Patches/VRRigCollderPatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+
+namespace SolidMonkeys.Patches
+{
+	[HarmonyPatch(typeof(VRRig))]
+	[HarmonyPatch("Start", MethodType.Normal)]
+	internal class VRRigColliderPatch
+	{
+		public static bool ModEnabled { get; set; }
+
+		private static void Postfix(VRRig __instance)
+		{
+			if (__instance.isOfflineVRRig)
+				return;
+
+			Photon.Pun.PhotonView photView = __instance.photonView;
+			if (photView != null && photView.IsMine)
+				return;
+
+			var cc = __instance.gameObject.AddComponent<ColliderController>();
+			cc.enabled = ModEnabled;
+
+			// Debug.Log("adding collider component");
+		}
+	}
+}
